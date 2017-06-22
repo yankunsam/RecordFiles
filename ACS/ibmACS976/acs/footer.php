c72ff3f39197e70403b17a6346f00c27247f4276
sam <samyankun@gmail.com>
2017-06-22 11:41:06 +0800

    Remove ACS code
    There is no need for a backup.

--- ACS/ibmACS976/acs/footer.php
+++
@@ -1,4 +0,0 @@
-<br>
-<div id="footer">
-Copyright &copy; IBM 2016
-</div> 
--- ACS/ibmACS976/acs/header.php
+++
@@ -1,6 +0,0 @@
-<h3 style="color:white;">Ken Goldman, IBM Research, kgoldman@us.ibm.com<br>
-<?php
-ini_set('display_errors', 1);
-echo "Attestation Server: " . gethostname()
-?>
-</h3>
